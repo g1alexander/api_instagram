352e3c12681d5c0b3d01e60e5d81a955800d1813
g1alexander <>
2020-07-02 12:19:02 -0500

    implementacion del api de instagram

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#myForm").submit(function(e){
+        e.preventDefault()
+        var username = $("#username").val()
+        $.instagramFeed({
+            'username': username,
+            'container' : "#instagram-feed-demo",
+            'display_profile':true,
+            'display_biography':true,
+            'items': 20,
+            'display_igtv':false
+
+        })
+    })
+})
